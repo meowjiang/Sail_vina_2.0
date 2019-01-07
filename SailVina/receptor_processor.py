7f00c0d434d36428dd4d421013ecd307e27849e3
beikwx <1987627818@qq.com>
2019-01-08 00:40:33 +0800

    解决完成准备受体逻辑。可以提取指定配体。

--- SailVina/receptor_processor.py
+++ SailVina/receptor_processor.py
@@ -1,6 +1,6 @@
 from Bio.PDB.PDBParser import PDBParser
-from Bio.PDB import Structure
-from Bio.PDB.PDBIO import PDBIO
+from Bio.PDB import Structure, Model, Chain, Select
+from Bio.PDB import PDBIO
 
 
 class ReceptorProcessor(object):
@@ -19,3 +19,118 @@ class ReceptorProcessor(object):
         for model in models:
             times += 1
         return times
+
+    @staticmethod
+    def get_model_ids(structure: Structure):
+        models = structure.get_models()
+        model_list = []
+        for model in models:
+            model_list.append(model.get_id())
+        return model_list
+
+    @staticmethod
+    def get_chain_nums(model: Model):
+        times = 0
+        chains = model.get_chains()
+        for chain in chains:
+            times += 1
+        return times
+
+    @staticmethod
+    def get_chain_ids(model: Model):
+        chains = model.get_chains()
+        chain_list = []
+        for chain in chains:
+            chain_list.append(chain.get_id())
+        return chain_list
+
+    @staticmethod
+    def get_het_nums(chain: Chain):
+        times = 0
+        residues = chain.get_residues()
+        for residue in residues:
+            residue_id = residue.get_id()
+            hetfield = residue_id[0]
+            if hetfield != " " and hetfield != "W":
+                times += 1
+        return times
+
+    @staticmethod
+    def get_het_ids(chain: Chain):
+        residues = chain.get_residues()
+        residue_list = []
+        for residue in residues:
+            residue_id = residue.get_id()
+            hetfield = residue_id[0]
+            if hetfield != " " and hetfield != "W":
+                residue_list.append(hetfield)
+        return residue_list
+
+    @staticmethod
+    def get_het_idname(hetname, chain: Chain):
+        residues = chain.get_residues()
+        for residue in residues:
+            residue_id = residue.get_id()
+            hetfield = residue_id[0]
+            if hetfield != " " and hetfield != "W":
+                if residue_id[0] == hetname:
+                    return residue_id
+        else:
+            return None
+
+
+class LigandExtractor(object):
+    structure = None
+    model_num = None
+    chain_name = None
+    residue_name = None
+
+    model = None
+    chain = None
+    ligand = None
+
+    def __init__(self, structure, model_num, chain_name, residue_name):
+        LigandExtractor.structure = structure
+        LigandExtractor.model_num = model_num
+        LigandExtractor.chain_name = chain_name
+        LigandExtractor.residue_name = residue_name
+
+    def extract_ligand(self, save_path):
+        # 拿到model对象
+        LigandExtractor.model = LigandExtractor.structure[LigandExtractor.model_num]
+        # 拿到chain对象
+        LigandExtractor.chain = LigandExtractor.model[LigandExtractor.chain_name]
+        # 拿到配体对象
+        for protein_res in self.chain.child_list:
+            if LigandExtractor.residue_name == protein_res.resname:
+                LigandExtractor.ligand = protein_res
+
+        class LigandSelect(Select):
+
+            def accept_model(self, model):
+                if model == LigandExtractor.model:
+                    return 1
+                else:
+                    return 0
+
+            def accept_chain(self, chain):
+                if chain == LigandExtractor.chain:
+                    return 1
+                else:
+                    return 0
+
+            def accept_residue(self, residue):
+                if residue == LigandExtractor.ligand:
+                    return 1
+                else:
+                    return 0
+
+        io = PDBIO()
+        io.set_structure(self.structure)
+        io.save(save_path + "/%s_%s.pdb" % (LigandExtractor.chain_name, LigandExtractor.residue_name), LigandSelect())
+
+
+if __name__ == '__main__':
+    s = PDBParser().get_structure("3ln1", "D:/Desktop/3ln1.pdb")
+    extractor = LigandExtractor(s, 0, "A", "CEL")
+    extractor.extract_ligand("D:/Desktop")
---
+++ active_site.py
@@ -0,0 +1,90 @@
+import Bio.PDB
+import numpy
+from Bio.PDB import PDBIO
+
+
+def residue_dist_to_ligand(protein_residue, ligand_residue):
+    # Returns distance from the protein C-alpha to the closest ligand atom
+    dist = []
+    for atom in ligand_residue:
+        if "CA" in protein_residue:
+            vector = protein_residue["CA"].coord - atom.coord
+            dist.append(numpy.sqrt(numpy.sum(vector * vector)))
+            return min(dist)
+
+
+def get_ligand_by_name(residue_name, model):
+    # Extract ligands from all chains in a model by its name
+    global ligands
+    ligands = {}
+    chains = model.child_dict
+    for c in chains:
+        ligands[c] = []
+        for protein_res in chains[c].child_list:
+            if protein_res.resname == residue_name:
+                ligands[c].append(protein_res)
+    return ligands
+
+
+def get_ligand_by_chain(chain_name, model):
+    # Extract all ligand residues from given chain name
+    global ligands
+    ligands = {}
+    ligands[chain_name] = []
+    chains = model.child_dict
+    for protein_res in chains[chain_name].child_list:
+        ligands[chain_name].append(protein_res)
+    return ligands
+
+
+def active_site(ligands, distance, model):
+    # Prints out residues located at a given distance from ligand
+    chains = model.child_dict
+    for group in ligands.values():
+        for ligand_res in group:
+            print("ligand residue: " + ligand_res.resname, ligand_res.id[1])
+            for c in chains:
+                for protein_res in chains[c].child_list:
+                    if protein_res not in group:
+                        dist = residue_dist_to_ligand(protein_res, ligand_res)
+                        if dist and dist < distance:
+                            print(protein_res.resname, protein_res.id[1], dist)
+
+
+def save_ligand(structure, filename):
+    # Saves ligand to a filename.pdb
+    Select = Bio.PDB.Select
+
+    class LigandSelect(Select):
+        def accept_residue(self, residue):
+            for group in ligands.values():
+                if residue in group:
+                    return 1
+                else:
+                    return 0
+
+    io = PDBIO()
+    io.set_structure(structure)
+    io.save(filename + '.pdb', LigandSelect())
+
+
+"""
+Example
+pdb_code = "1E8W"
+pdb_filename = "1E8W.pdb"
+structure = Bio.PDB.PDBParser().get_structure(pdb_code, pdb_filename)
+model = structure[0]
+ligand = get_ligand_by_name('QUE', model)
+active_site(ligand, 5.0, model)
+
+structure2 = Bio.PDB.PDBParser().get_structure('4GRV', '4GRV.pdb')
+model2 = structure2[0]
+ligand2 = get_ligand_by_chain('B', model2)
+active_site(ligand2, 8.0, model2)
+save_ligand(structure2, 'ligand')
+"""
+
+structure = Bio.PDB.PDBParser().get_structure("2az5", "2az5.pdb")
+model = structure[0]
+ligand = get_ligand_by_name('GLY', model)
+print(ligand)
